f857fb0401c8ca3fa55a6d84a9c238fd42be5b46
Martin Hujer <mhujer@gmail.com>
2017-12-14 21:47:50 +0100

    design

--- components/Menu.js
+++ components/Menu.js
@@ -4,12 +4,12 @@ export default (props) => (
   <ul className="nav nav-pills ournavmenu">
     <li className="nav-item">
       <Link href="/">
-        <a className={`nav-link ${props.active === 'index' ? 'active' : ''}`}>Status</a>
+        <a className={`nav-link ${props.active === 'index' ? 'active' : ''}`}>Cluster Status</a>
       </Link>
     </li>
     <li className="nav-item">
       <Link href="/query">
-        <a className={`nav-link ${props.active === 'query' ? 'active' : ''}`} >Query UI</a>
+        <a className={`nav-link ${props.active === 'query' ? 'active' : ''}`} >Interactive Query UI</a>
       </Link>
     </li>
   </ul>
