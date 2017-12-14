132b8d3af041c5982398ff5b0496967aeede679f
Martin Hujer <mhujer@gmail.com>
2017-12-14 19:09:10 +0100

    connect?

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,16 @@
+let elasticsearch = require('elasticsearch');
+let client = new elasticsearch.Client({
+	host: '127.0.0.1:9200',
+	log: 'trace'
+});
+client.ping({
+	requestTimeout: 30000,
+}, function (error) {
+	if (error) {
+		console.error('elasticsearch cluster is down!');
+	} else {
+		console.log('All is well');
+	}
+});
+
 export default () => <div>Welcome to next.js!</div>
