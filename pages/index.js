b34cf5330fee176c24f29613e22c1d45011f2883
Martin Hujer <mhujer@gmail.com>
2017-12-14 19:36:17 +0100

    list indices

--- pages/index.js
+++ pages/index.js
@@ -4,22 +4,35 @@ import elasticsearch from 'elasticsearch'
 
 export default class App extends React.Component {
 
-  state = {data: null}
+  state = {data: []}
 
   async componentDidMount() {
     const client = new elasticsearch.Client({
-    	host: '127.0.0.1:9200',
-    	log: 'trace'
+      host: '127.0.0.1:9200',
+      log: 'trace'
     })
-    const data = await client.info({
-    	requestTimeout: 30000
+    const data = await client.cat.indices({
+      format: 'json',
+      requestTimeout: 30000
     })
     this.setState({data})
   }
 
   render() {
     return (
-      <pre>{JSON.stringify(this.state.data, null, 2)}</pre>
+      <div>
+        {
+          this.state.data.map(item =>
+            <div>
+              <h1>{item.index}</h1>
+              <div></div>
+              <small>{item.health}</small>
+            </div>
+          )
+        }
+
+        <pre>{JSON.stringify(this.state.data, null, 2)}</pre>
+      </div>
     )
   }
 
