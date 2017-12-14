6cca73ada5b0a8f371513fc268d875ba31321689
Minarik, Jaroslav <jaroslav.minarik@merck.com>
2017-12-14 19:20:45 +0100

    Hello world elastic

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,26 @@
-let elasticsearch = require('elasticsearch');
-let client = new elasticsearch.Client({
-	host: '127.0.0.1:9200',
-	log: 'trace'
-});
-const response = client.info({
-	requestTimeout: 30000,
-});
-
-response.then((res) => {
-	console.dir(res);
-});
+import React from 'react'
+import elasticsearch from 'elasticsearch'
 
 
-export default () => <div>Welcome to next.js!</div>
+export default class App extends React.Component {
+
+  state = {data: null}
+
+  async componentDidMount() {
+    const client = new elasticsearch.Client({
+    	host: '127.0.0.1:9200',
+    	log: 'trace'
+    })
+    const data = await client.info({
+    	requestTimeout: 30000
+    })
+    this.setState({data})
+  }
+
+  render() {
+    return (
+      <pre>{JSON.stringify(this.state.data, null, 2)}</pre>
+    )
+  }
+
+}
